17b2ea3f24ed52ed69adf217062fefa30829ec15
JKells99 <j_kelloway@hotmail.com>
2023-05-16 09:00:40 -0230

    Initial commot

---
+++ src/main/java/com/keyin/example/Example.java
@@ -0,0 +1,14 @@
+package com.keyin.example;
+
+public class Example {
+
+    public Integer multiply(int x, int y){
+        return x * y ;
+    }
+
+    public String format(String stringToFormat){
+        return stringToFormat.trim().replaceAll(" ", ",");
+    }
+
+
+}
---
+++ src/test/java/com/keyin/example/ExampleTest.java
@@ -0,0 +1,25 @@
+package com.keyin.example;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    public void testMultiply(){
+        Example exampleUnderTest = new Example();
+        Assertions.assertEquals(10,exampleUnderTest.multiply(10,1));
+        Assertions.assertNotEquals(10,exampleUnderTest.multiply(13,1));
+
+    }
+
+    @Test
+    public void testFormat(){
+        Example exampleUnderTest = new Example();
+
+        String source = "Test string formatting method";
+        String expectedResult = "Test,string,formatting,method";
+
+        Assertions.assertEquals(expectedResult,exampleUnderTest.format(source));
+
+    }
+}
