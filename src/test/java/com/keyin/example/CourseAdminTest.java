ce22166066ae3e35c28dd2c4c75f3d069edf49f7
JKells99 <j_kelloway@hotmail.com>
2023-05-23 09:42:33 -0230

    added new test to demo actions

--- src/test/java/com/keyin/example/CourseAdminTest.java
+++ src/test/java/com/keyin/example/CourseAdminTest.java
@@ -34,4 +34,35 @@ public class CourseAdminTest {
 
 
     }
+
+
+    @Test
+    @DisplayName("Test Course List")
+    public void testGetCourseListByStudent_Failing(){
+        CourseAdmin courseAdmin = new CourseAdmin();
+
+        Student student = new Student();
+
+        student.setId(1);
+        student.setName("Jordan");
+
+        Course course = new Course();
+
+        course.setId(1);
+        course.setName("SWDAT");
+
+        courseAdmin.addCourseToStudent(student,course);
+
+        Student studentToFind = new Student();
+        studentToFind.setId(1);
+        studentToFind.setName("Jordan");
+
+        Course courseToFind = new Course();
+        courseToFind.setId(1);
+        courseToFind.setName("JavaScript");
+
+        Assertions.assertTrue(courseAdmin.getCourseListByStudent(studentToFind).contains(courseToFind));
+
+
+    }
 }
